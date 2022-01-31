94c03a0bd6bcfb98b235ad934ffb10283dc1aec5
Shivam Rai <63858813+Shivam00100@users.noreply.github.com>
2022-01-31 20:43:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    if(principal<=0){alert("Enter a positive number")}
+    if(principal<=0){alert("Enter a positive number")
+    
+    document.getElementById("principal").focus();
+    return compute;
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     
     var year = new Date().getFullYear()+parseInt(years);
-    var result = document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("text1").innerHTML="If you deposit";
+    document.getElementById("principal1").innerHTML=principal+",";
+    document.getElementById("text2").innerHTML="at an interest rate of";
+    document.getElementById("rate1").innerHTML=rate;
+    document.getElementById("text3").innerHTML="You will receive an amount of";
+    document.getElementById("interest1").innerHTML=interest+",";
+    document.getElementById("text4").innerHTML="in the year";
+    document.getElementById("year1").innerHTML=year;
 }
 function updateRate() 
 {
